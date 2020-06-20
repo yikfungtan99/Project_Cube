748f8279bab86baf7175057c16f2b9ea19b89f65
NicholasTiew <nicholastiewofficial@gmail.com>
2020-06-20 16:14:17 +0800

    Camera Zoom Update
    Changed the value of these
    Rotate Distance = 9
    Examine Distance = 6.5

---
+++ Assets/Scripts/TouchInputs.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInputs : MonoBehaviour
+{
+
+    private bool _tap, _doubleTap, _swipeLeft, _swipeRight, _swipeUp, _swipeDown;
+    
+    //double tap
+    [SerializeField] private float doubleTapDelta = 0.5f;
+    
+    private float _lastTap;
+    
+    //swipe data
+    private Vector2 _swipeDelta, _startTouch;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void UpdateInput()
+    {
+        if (Input.touches.Length != 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                _tap = true;
+                _startTouch = Input.mousePosition;
+                _doubleTap = Time.time - _lastTap < doubleTapDelta;
+                _lastTap = Time.deltaTime;
+            }
+            
+        }
+        
+    }
+}

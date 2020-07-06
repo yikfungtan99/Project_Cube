2cf3a9dcbd8b3738e80bae02e23bf7956ce6c38e
yikfungtan99 <50047634+yikfungtan99@users.noreply.github.com>
2020-07-06 13:02:52 +0800

    v0.16
    Added UI Graphics

---
+++ Assets/mainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene (1);
+    }
+
+    public void SettingButton()
+    {
+        
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene (3);
+    }
+
+    public void backButton()
+    {
+        SceneManager.LoadScene (0);
+    }
+}

0d0f833a8c96bc80261aa2498bafc60d58485f3e
yikfungtan99 <50047634+yikfungtan99@users.noreply.github.com>
2020-08-14 13:19:11 +0800

    v0.20e
    Added loading screen

--- Assets/Scripts/PlayerCube.cs
+++ Assets/Scripts/PlayerCube.cs
@@ -136,7 +136,7 @@ public class PlayerCube : MonoBehaviourPun, IPunInstantiateMagicCallback
         if (_allPuzzleInitialized && otherCube._allPuzzleInitialized)
         {
             if (_gameStarted) return;
-            print("Start");
+            GameObject.Find("LoadCanvas").transform.GetChild(0).gameObject.SetActive(false);
             foreach (var t in modules)
             {
                 t.puzzleManager.PuzzleStart();
